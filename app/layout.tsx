f850b6546cf990dbfe2b7c1965c5ba6dd8850228
Zhi Su <116618848+Advil4@users.noreply.github.com>
2024-04-26 12:35:31 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "风雪载途",
+  description: "晚上吃啥",
   viewport: {
     width: "device-width",
     initialScale: 1,
